9cb950f126fcfeeaa1f41c56adec22f02143e92c
Selvin Ortiz <selvin@selvin.co>
2013-07-05 14:40:48 -0500

    Adds dynamic version number to settings template and adds test function for templates.

--- variables/SpamGuardVariable.php
+++ variables/SpamGuardVariable.php
@@ -3,22 +3,52 @@ namespace Craft;
 
 class SpamGuardVariable
 {
-	public function isSpam( $data=array() )
+	public function getVersion()
 	{
-		try
-		{
-			$model = SpamGuardModel::populateModel($data);
+		return craft()->plugins->getPlugin(SpamGuardPlugin::PLUGIN_HANDLE)->getVersion();
+	}
 
-			if ( $model->validate() )
+	//--------------------------------------------------------------------------------
+	
+	/**
+	 * isSpam()
+	 *
+	 * Provided for testing withing a template only
+	 */
+	public function isSpam( $data, $onSuccess=false, $onFailure=false)
+	{
+		$params = array(
+			'data'	=> array(
+				'content'	=> arrayGet('content', $data),
+				'author'	=> arrayGet('author', $data),
+				'email'		=> arrayGet('email', $data)
+			),
+			'onSuccess'		=> $onSuccess,
+			'onFailure'		=> $onFailure
+		);
+
+		$model = SpamGuardModel::populateModel($params['data']);
+
+		if ($model->validate())
+		{
+			try
 			{
-				return (bool) craft()->spamGuard->isSpam($model);
+				if (craft()->spamGuard->isSpam($model))
+				{
+					return 'You got totally spammed!!!';
+				}
+				else
+				{
+					return 'The inbox will be pleased, no spam here!';
+				}
+			}
+			catch (\Exception $e)
+			{
+				return 'Something we totally nuts:<br>'.$e->getMessage();
 			}
 
-			return false;
 		}
-		catch (\Exception $e)
-		{
-			return false;
-		}
+
+		return 'Data is not good enough dude!';
 	}
 }
